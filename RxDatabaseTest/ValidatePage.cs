b6172fe3bc6ece8b8d9a2a8ffdf81e9e5e526a6d
shakthy <shakthy@gmail.com>
2020-04-21 18:58:27 +0100

    8th

--- RxDatabaseTest/ValidatePage.cs
+++ RxDatabaseTest/ValidatePage.cs
@@ -120,53 +120,61 @@ namespace RxDatabaseTest
             Mouse.ScrollWheel(-720);
             Delay.Milliseconds(500);
             
-            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='Broadbandonly') on item 'VirginMediaOfficialSite.Broadbandonly'.", repo.VirginMediaOfficialSite.BroadbandonlyInfo, new RecordItemIndex(11));
+            Report.Screenshot(ReportLevel.Info, "User", "", null, false, new RecordItemIndex(11));
+            
+            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='Broadbandonly') on item 'VirginMediaOfficialSite.Broadbandonly'.", repo.VirginMediaOfficialSite.BroadbandonlyInfo, new RecordItemIndex(12));
             Validate.Attribute(repo.VirginMediaOfficialSite.BroadbandonlyInfo, "InnerText", "Broadbandonly");
             Delay.Milliseconds(100);
             
-            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='M100\nFibre Broadband') on item 'VirginMediaOfficialSite.M100FibreBroadband'.", repo.VirginMediaOfficialSite.M100FibreBroadbandInfo, new RecordItemIndex(12));
+            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='M100\nFibre Broadband') on item 'VirginMediaOfficialSite.M100FibreBroadband'.", repo.VirginMediaOfficialSite.M100FibreBroadbandInfo, new RecordItemIndex(13));
             Validate.Attribute(repo.VirginMediaOfficialSite.M100FibreBroadbandInfo, "InnerText", "M100\nFibre Broadband");
             Delay.Milliseconds(100);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaOfficialSiteInternet.ClientCaption' at 1890;28.", repo.VirginMediaOfficialSiteInternet.ClientCaptionInfo, new RecordItemIndex(13));
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Scroll Vertical by 720 units.", new RecordItemIndex(14));
+            Mouse.ScrollWheel(720);
+            Delay.Milliseconds(500);
+            
+            Report.Screenshot(ReportLevel.Info, "User", "", null, false, new RecordItemIndex(15));
+            
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaOfficialSiteInternet.ClientCaption' at 1890;28.", repo.VirginMediaOfficialSiteInternet.ClientCaptionInfo, new RecordItemIndex(16));
             repo.VirginMediaOfficialSiteInternet.ClientCaption.Click("1890;28");
             Delay.Milliseconds(200);
             
-            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaOfficialSite.MyVirginMedia' at 64;15.", repo.VirginMediaOfficialSite.MyVirginMediaInfo, new RecordItemIndex(14));
+            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaOfficialSite.MyVirginMedia' at 64;15.", repo.VirginMediaOfficialSite.MyVirginMediaInfo, new RecordItemIndex(17));
             //repo.VirginMediaOfficialSite.MyVirginMedia.Click("64;15");
             //Delay.Milliseconds(200);
             
-            //Report.Log(ReportLevel.Info, "Delay", "Waiting for 30s.", new RecordItemIndex(15));
+            //Report.Log(ReportLevel.Info, "Delay", "Waiting for 30s.", new RecordItemIndex(18));
             //Delay.Duration(30000, false);
             
-            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='Help') on item 'VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.Help'.", repo.VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.HelpInfo, new RecordItemIndex(16));
+            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='Help') on item 'VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.Help'.", repo.VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.HelpInfo, new RecordItemIndex(19));
             //Validate.Attribute(repo.VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.HelpInfo, "InnerText", "Help");
             //Delay.Milliseconds(100);
             
-            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='Expanding our network\n') on item 'VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.ExpandingOurNetwork'.", repo.VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.ExpandingOurNetworkInfo, new RecordItemIndex(17));
+            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='Expanding our network\n') on item 'VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.ExpandingOurNetwork'.", repo.VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.ExpandingOurNetworkInfo, new RecordItemIndex(20));
             //Validate.Attribute(repo.VirginMediaMyVirginMedia.UnifiedHeaderLevelOne.ExpandingOurNetworkInfo, "InnerText", "Expanding our network\n");
             //Delay.Milliseconds(100);
             
-            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='For the home\n') on item 'VirginMediaMyVirginMedia.ForTheHome'.", repo.VirginMediaMyVirginMedia.ForTheHomeInfo, new RecordItemIndex(18));
+            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='For the home\n') on item 'VirginMediaMyVirginMedia.ForTheHome'.", repo.VirginMediaMyVirginMedia.ForTheHomeInfo, new RecordItemIndex(21));
             //Validate.Attribute(repo.VirginMediaMyVirginMedia.ForTheHomeInfo, "InnerText", "For the home\n");
             //Delay.Milliseconds(100);
             
-            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Href='https://store.virginmedia.com/index.html') on item 'VirginMediaMyVirginMedia.ForTheHome'.", repo.VirginMediaMyVirginMedia.ForTheHomeInfo, new RecordItemIndex(19));
+            //Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Href='https://store.virginmedia.com/index.html') on item 'VirginMediaMyVirginMedia.ForTheHome'.", repo.VirginMediaMyVirginMedia.ForTheHomeInfo, new RecordItemIndex(22));
             //Validate.Attribute(repo.VirginMediaMyVirginMedia.ForTheHomeInfo, "Href", "https://store.virginmedia.com/index.html");
             //Delay.Milliseconds(100);
             
-            //Report.Screenshot(ReportLevel.Info, "User", "", null, false, new RecordItemIndex(20));
+            //Report.Screenshot(ReportLevel.Info, "User", "", null, false, new RecordItemIndex(23));
             
-            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaMyVirginMedia.ForBusiness' at 64;20.", repo.VirginMediaMyVirginMedia.ForBusinessInfo, new RecordItemIndex(21));
+            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaMyVirginMedia.ForBusiness' at 64;20.", repo.VirginMediaMyVirginMedia.ForBusinessInfo, new RecordItemIndex(24));
             //repo.VirginMediaMyVirginMedia.ForBusiness.Click("64;20");
             //Delay.Milliseconds(200);
             
-            //Report.Log(ReportLevel.Info, "Delay", "Waiting for 15s.", new RecordItemIndex(22));
+            //Report.Log(ReportLevel.Info, "Delay", "Waiting for 15s.", new RecordItemIndex(25));
             //Delay.Duration(15000, false);
             
-            //Report.Screenshot(ReportLevel.Info, "User", "", null, false, new RecordItemIndex(23));
+            //Report.Screenshot(ReportLevel.Info, "User", "", null, false, new RecordItemIndex(26));
             
-            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaBusinessBusinessAndPub.ClientCaption' at 1881;24.", repo.VirginMediaBusinessBusinessAndPub.ClientCaptionInfo, new RecordItemIndex(24));
+            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'VirginMediaBusinessBusinessAndPub.ClientCaption' at 1881;24.", repo.VirginMediaBusinessBusinessAndPub.ClientCaptionInfo, new RecordItemIndex(27));
             //repo.VirginMediaBusinessBusinessAndPub.ClientCaption.Click("1881;24");
             //Delay.Milliseconds(200);
             
